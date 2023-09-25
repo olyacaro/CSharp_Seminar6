799e17deaa8fca9df570bfd5d75527989222c13d
Olga <olyacaro@mail.ru>
2023-09-25 16:14:24 +0300

    add Напишите программу, которая принимает на вход число и выдает количество цифр в числе. 456 -> 3 78 -> 2 89126 -> 5

---
+++ GB/CSharp_Seminar4/Task26/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 
+Напишите программу, которая принимает на вход число
+и выдает количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+int num = GetNumber(" ");
+int i = 0;
+int GetNumber(string message)
+{
+    Console.Write($"Введите число {message}: ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+for( ; num > 0; num/=10)
+    {
+        i++;
+    }
+Console.WriteLine("Количество цифр в числе " +i);
