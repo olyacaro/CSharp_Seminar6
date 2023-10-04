a93ff8623c38b0e3a76023d28d51acc942f817c5
Olga <olyacaro@mail.ru>
2023-10-04 17:43:33 +0300

    add Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123 [5, 18, 123, 6, 2] -> 1 [1, 2, 3, 6, 2] -> 0 [10, 11, 12, 13, 14] -> 5

---
+++ GB/CSharp_Seminar5/Task35/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 35: Задайте одномерный массив из 123 случайных чисел.
+Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1 
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5 */
+
+int[] getRandomArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+int schet(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] >= 10 && array[i] <= 99)
+        {
+            count++;
+        }
+    }
+return count;
+}
+int[] array = getRandomArray(5, -123, 123);
+Console.WriteLine(string.Join(", ", array));
+int result = schet(array);
+Console.WriteLine(result);
