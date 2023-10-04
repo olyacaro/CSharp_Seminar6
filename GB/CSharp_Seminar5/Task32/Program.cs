c1c09620ac23a1a6ce349d51c2409fae0e3507cd
Olga <olyacaro@mail.ru>
2023-10-04 17:20:43 +0300

    add Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот. [-4, -8, 8, 2] -> [4, 8, -8, -2]

---
+++ GB/CSharp_Seminar5/Task32/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 32: Напишите программу замена элементов массива: 
+положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+*/
+
+int[] getRandomArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+void plusMinus(int[] array)
+{
+    for(int i= 0; i < array.Length; i++)
+    {
+        array[i] = array [i] * -1;
+    }
+}
+
+int[] array = getRandomArray(4, -9, 9);
+Console.WriteLine(string.Join(", ", array));
+plusMinus(array);
+Console.WriteLine(string.Join(", ", array));
+
