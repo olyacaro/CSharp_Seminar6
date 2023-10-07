82e4a99c6b5412cc4d7185300fbc62900c566570
Olga <olyacaro@mail.ru>
2023-10-07 19:53:17 +0300

    add Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.) [1 2 3 4 5] -> [5 4 3 2 1] [6 7 3 6] -> [6 3 7 6]

---
+++ GB/CSharp_Seminar6/Task39/Program.cs
@@ -0,0 +1,31 @@
+﻿/* 
+Задача 39: Напишите программу, которая перевернёт одномерный массив 
+(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+[1 2 3 4 5] -> [5 4 3 2 1] 
+[6 7 3 6] -> [6 3 7 6]
+*/
+
+int[] getRandomArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void Revers(int[] arr)
+{
+    int temp = 0;
+    for(int i = 0; i < arr.Length / 2; i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    } 
+}
+int[] arr = getRandomArray(10, 1, 10);
+Console.WriteLine(string.Join(" ", arr));
+Revers(arr);
+Console.WriteLine(string.Join(" ", arr));
