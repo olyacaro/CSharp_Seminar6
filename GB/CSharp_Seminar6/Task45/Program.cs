f55a3c62358818abb8e02b913e07f055dc505169
Olga <olyacaro@mail.ru>
2023-10-11 01:05:31 +0300

    add Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.

---
+++ GB/CSharp_Seminar6/Task45/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 45: Напишите программу,
+которая будет создавать копию заданного массива 
+с помощью поэлементного копирования.*/
+
+
+int[] getRandomArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+int[] getNewMas(int[] arr)
+{
+    int[] array = new int[arr.Length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = arr[i];
+    }
+    return array;
+}
+
+int[] Choto = getRandomArray(10, -100, 100);
+Console.WriteLine(string.Join(" ", Choto));
+int[] ChotoToto = getNewMas(Choto);
+Console.WriteLine(string.Join(" ", ChotoToto));
